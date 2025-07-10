4aabf942ed9c0ac00649c229c60bb79445623b12
Anna Burnett <annairishburnett@gmail.com>
2025-07-10 19:02:20 -0400

    added testimonial slider, testimonials, and css formatting for it

--- assets/js/main.js
+++ assets/js/main.js
@@ -207,6 +207,27 @@
     }
   });
 
+  //Testimonial Slider
+//Source: https://youtu.be/iIwALoXqJ5c?si=gnFYKW2d5cdmxWBK
+var swiper = new Swiper(".mySwiper", {
+	grabCursor: true,
+	centeredSlides: true,
+	autoplay: {
+	  delay: 10000,
+	  disableOnInteraction: false,
+	},
+	loop: true,
+	pagination: {
+	  el: ".swiper-pagination",
+	  clickable: true,
+	},
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+  });
+  
+
   /**
    * Porfolio isotope and filter
    */
